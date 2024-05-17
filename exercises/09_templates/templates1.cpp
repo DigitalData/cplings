21d4b08973caae14f1c9078ba852aad77db505c1
xtravers <legoxavierlocketravers@gmail.com>
2024-05-17 17:16:44 +1000

    Update templates1.cpp

--- exercises/09_templates/templates1.cpp
+++ exercises/09_templates/templates1.cpp
@@ -12,14 +12,18 @@
 // function so that it could support generic inputs (both int and double).
 // Use a template parameter for the generic T returned by the function max.
 
-int max(int i0, int i1) {
-    if (i0 > i1) {
-        return i0;
-    }
-    else {
-        return i1;
-    }
+template <typename T> T max(T i0, T i1) {
+    return {i0 > i1 ? i0:i1};
 }
+
+// int max(int i0, int i1) {
+//     if (i0 > i1) {
+//         return i0;
+//     }
+//     else {
+//         return i1;
+//     }
+// }
 
 
 constexpr double ALMOST_EIGHT = 8.10;
@@ -28,11 +32,11 @@ constexpr double PI = 3.1415;
 std::pair<int, double> test_templates1() {
     int i0 = -4;
     int i1 = 3;
-    int max_i = max(i0, i1);
+    int max_i = max<int>(i0, i1);
 
     double d0 = ALMOST_EIGHT;
     double d1 = PI;
-    double max_d = max(d0, d1);
+    double max_d = max<double>(d0, d1);
 
     return { max_i, max_d };
 }
