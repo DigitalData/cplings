5a8b13d821912878c52c31aa0c0ef8ed082ff244
xtravers <legoxavierlocketravers@gmail.com>
2024-05-17 17:14:51 +1000

    Update classes3.cpp

--- exercises/05_classes/classes3.cpp
+++ exercises/05_classes/classes3.cpp
@@ -26,17 +26,21 @@ protected:
     Point center_;
 
 public:
-    Shape(const Point & centre = point_zero) : center_(centre) { }
+    Shape(const Point & centre = point_zero) : center_(centre) {
+        center_ = centre;
+    }
 
     Point center() const {
-        return point_zero;
+        return center_;
     }
 };
 
 
 class Circle : public Shape {
 public:
-    Circle(const Point& centre = point_zero) { } // Fix: add a Call to parent constructor
+    Circle(const Point& centre = point_zero) : Shape(centre) {
+
+    } // Fix: add a Call to parent constructor
 
     Point center() const {
         return center_;
